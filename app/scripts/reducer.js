8d329c2338f15b5c4fa43ec94bb72398afd7971e
Brian Fegan <brianfegan@gmail.com>
2016-01-13 20:19:36 -0700

    remove global action creator; move url actions into routes

--- app/scripts/reducer.js
+++ app/scripts/reducer.js
@@ -4,17 +4,13 @@ import constants from './constants';
 import products from './components/products/reducer';
 import categories from './components/filtersList/reducer';
 
-function url(state = Map(), action) {
+let reducers = [categories, products];
+
+export default function reducer(state = Map(), action) {
   switch (action.type) {
     case constants.URL_CHANGE:
       return state.set('url', Map({params:action.params, query:action.query, path:action.path}));
   }
-  return state;
-}
-
-let reducers = [categories, products, url];
-
-export default function reducer(state = Map(), action) {
   for (let i=0; i<reducers.length; i++) {
     let nextState = reducers[i](state, action);
     if (!is(state, nextState)) return nextState;
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -2,14 +2,31 @@ import React from 'react';
 import {Route} from 'react-router';
 
 // Require App Modules
-import {setUrlState} from './actionCreators';
 import Wrapper from './layouts/wrapper';
 import Products from './components/products/component';
+import constants from './constants';
+import {getStore} from './store';
+
+let currentPath;
+
+function getNormalizedPath(path) {
+  let pathLen = path.length;
+  let lastChar = path.charAt(pathLen-1);
+  if (lastChar === '/') {
+    path = path.substring(0, (pathLen-2));
+  }
+  return path;
+}
 
 let App = React.createClass({
   componentWillReceiveProps: function(props){
-    let path = props.location.pathname + props.location.search;
-    setUrlState(props.params, props.location.query, path);
+    let location = props.location;
+    let path = getNormalizedPath(location.pathname + location.search);
+    if (path !== currentPath) {
+      currentPath = path;
+      let store = getStore();
+      store.dispatch({type:constants.URL_CHANGE, params:props.params, query:location.query, path:path});
+    }
   },
   render: function() {
     return (
